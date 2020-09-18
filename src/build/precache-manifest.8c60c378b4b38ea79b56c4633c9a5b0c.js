45b93aed862b38e4e143d1862bbcc3c81c947d3a
Lovejit352 <singh.lovejit00@gmail.com>
2020-09-17 20:19:34 -0400

    first commit

---
+++ src/build/precache-manifest.8c60c378b4b38ea79b56c4633c9a5b0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8612efe0520ff6783753b5e3511d3426",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f91ef21ad9c4e96cc610",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "33803bca1eb6d23093f9",
+    "url": "/static/js/2.def41a19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.def41a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f91ef21ad9c4e96cc610",
+    "url": "/static/js/main.d2d0ef57.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
